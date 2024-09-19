eebe6ef991d9d1e9cedfce5d39aed166620df1c5
Nesrux <joaomarcosdevs@gmail.com>
2024-09-19 18:07:07 -0300

    fix: resolve problema da logo da netflix

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 export default function Header() {
   return (
-    <header className='lg:py10 fixed top-0 z-50 flex w-full items-center justify-between bg-red-500 px-4 py-4 lg:px-10'>
+    <header className='lg:py10 fixed top-0 z-50 flex w-full items-center justify-between px-4 py-4 lg:px-10'>
       <img
-        src='https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/200px-Netflix_2015_logo.svg.png'
+        src='https://rb.gy/ulxxee'
         alt='Netflix'
         width={120}
         height={120}
