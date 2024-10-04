5de228c74ab520a670905d89763a1d1b1a1d0150
Nesrux <joaomarcosdevs@gmail.com>
2024-10-04 17:58:39 -0300

    chore: roteamento do next

---
+++ src/app/lib/settings.tsx
@@ -0,0 +1,29 @@
+export const getAppSettings = (): Promise<{
+  theme: string;
+  language: string;
+}> => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve({
+        theme: 'dark',
+        language: 'en',
+      });
+    }, 5000);
+  });
+};
+
+export const getUserInfo = (): Promise<{
+  name: string;
+  email: string;
+  age: number;
+}> => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve({
+        name: 'João ninguém',
+        email: 'joaoNinguem@gmail.com',
+        age: 29,
+      });
+    }, 2000);
+  });
+};
