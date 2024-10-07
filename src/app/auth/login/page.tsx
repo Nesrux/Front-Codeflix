60ba3339826a5cb300b725fe9ee67b6b15d110ec
Nesrux <joaomarcosdevs@gmail.com>
2024-10-07 18:50:14 -0300

    chore: pagina de login

---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Login() {
+  return (
+    <div
+      className='bg-cover bg-center bg-no-repeat text-white'
+      style={{ backgroundImage: 'url(/background.jpg)' }}
+    >
+      <div className='flex min-h-screen flex-col items-center justify-center py-2'>
+        Login ppage
+      </div>
+    </div>
+  );
+}
