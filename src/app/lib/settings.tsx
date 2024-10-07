0d403e21ee5b9177090cc3a83df80bd2ab851dfc
Nesrux <joaomarcosdevs@gmail.com>
2024-10-07 18:33:58 -0300

    chore: mais componentes

--- src/app/lib/settings.tsx
+++ src/app/lib/settings.tsx
@@ -1,3 +1,31 @@
+export interface Address {
+  street: string;
+  suite: string;
+  city: string;
+  zipcode: string;
+  geo: Geo;
+}
+export interface Geo {
+  lat: string;
+  lng: string;
+}
+
+export interface User {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: Address;
+  phone: string;
+  website: string;
+  company: Company;
+}
+export interface Company {
+  name: string;
+  catchPhrase: string;
+  bs: string;
+}
+
 export const getAppSettings = (): Promise<{
   theme: string;
   language: string;
@@ -27,3 +55,11 @@ export const getUserInfo = (): Promise<{
     }, 2000);
   });
 };
+
+export async function getUserByid(id: string): Promise<User> {
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/users/${id}`
+  );
+  const User = await response.json();
+  return User;
+}
---
+++ src/app/settings/user/[id]/not-found.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function NotFound() {
+  <div>
+    <h1>The user you are looking for is not found</h1>
+  </div>;
+}
