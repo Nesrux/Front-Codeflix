d86fed7a0476fef6dc7195134fcce9fd7d2aa3cd
Nesrux <joaomarcosdevs@gmail.com>
2024-10-09 17:24:50 -0300

    chore: auth

--- src/app/auth/login/LoginForm.tsx
+++ src/app/auth/login/LoginForm.tsx
@@ -1,12 +1,40 @@
 'use client';
 
 import { AuthForm } from '@/app/components/authForm';
-import React from 'react';
+import { useRouter } from 'next/navigation';
+import { FormEvent, useState } from 'react';
 
+type ServerError = {
+  message: string;
+};
 export default function LoginForm() {
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    alert('submit');
+  const router = useRouter();
+  const [errors, setErrors] = useState<string[]>([]);
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const email = formData.get('email');
+    const password = formData.get('password');
+
+    try {
+      const response = await fetch('/auth/login/api', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ email, password }),
+      });
+
+      if (response.ok) {
+        router.push('/');
+        return;
+      }
+
+      const payload: ServerError[] = await response.json();
+      setErrors(payload.map((error) => error.message));
+    } catch (error) {
+      console.error(error);
+      setErrors(['An unknown error occurred.']);
+    }
   };
   return <AuthForm formType='login' onSubmit={handleSubmit} />;
 }
