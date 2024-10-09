50fee23fbf54b25f8b32b4e00b615ea98d4ba583
Nesrux <joaomarcosdevs@gmail.com>
2024-10-09 16:14:14 -0300

    refactor: alterando o nome do arquivo

--- src/app/components/auth.form.tsx
+++
@@ -1,8 +0,0 @@
-'use client';
-type AuthFormProps = {
-  formType: 'login' | 'register';
-  onSubmit: (e: React.FormEvent<HTMLFormElement>) => void;
-};
-export const AuthForm: React.FC<AuthFormProps> = ({ formType, onSubmit }) => {
-  return;
-};
