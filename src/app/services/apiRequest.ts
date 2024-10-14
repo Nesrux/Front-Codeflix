d2e780f1b2fee2af0aa5ec0f4864f79c9f41ee9f
Nesrux <joaomarcosdevs@gmail.com>
2024-10-14 16:02:51 -0300

    chore: service de requests para api

--- src/app/services/apiRequest.ts
+++ src/app/services/apiRequest.ts
@@ -1,4 +1,4 @@
-const API_URL = 'https://localhost:8080';
+const API_URL = process.env.API_URL || 'https://localhost:8080'
 // Featured
 // movies
 export interface ApiQueryParams {
@@ -14,13 +14,27 @@ export const defaultRequestOptions: RequestOptions = {
     page: 1,
     _limit: 10
 }
+export function buildQuerySting(params: ApiQueryParams){
+    const query = Object.entries(params)
+    .filter(([, value]) => value != undefined) 
+    .map(([key, value]) => [key, encodeURIComponent(String(value))])
 
-export async function apiRequest(endpoint: string,query: ApiQueryParams, options: RequestOptions) {
+    return `?${new URLSearchParams(Object.fromEntries(query)).toString()}`;
+}
+
+export async function apiRequest(
+    endpoint: string,
+    query: ApiQueryParams, 
+    options: RequestOptions) {
+    const mergeOptions: RequestOptions = {... defaultRequestOptions, ...options}
+    const queryString: String = buildQuerySting({...query, ...mergeOptions});
+
     try{
-    const response = fetch(`${API_URL}/${endpoint}`)
-    const data = await (await response).json();
-    return data;
+        const response =  await fetch(`${API_URL}/${endpoint}${queryString}`)
+        if(!response.ok){
+            throw new Error(`API request failed: ${response.statusText}`)
+        }
     }catch(error) {
-        console.log(error)
+      throw error
     }
 }
