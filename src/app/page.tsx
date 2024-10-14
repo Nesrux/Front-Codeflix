4cc2ee2f2068e8581a013bc1d6c33e8f44149dd2
Nesrux <joaomarcosdevs@gmail.com>
2024-10-14 17:15:16 -0300

    feat: adiciona banner dinamico

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import { Banner } from './components/Banner';
 import Header from './components/Header';
 import { MovieRow } from './components/MovieRow';
-import { getFeaturedMovie, getMovieById } from './services/movieService';
+import { getFeaturedMovie, getMovieById } from './services/MovieService';
 
 export default async function Home() {
-  const featuredMovie = getFeaturedMovie('101');
+  const randomNum = Math.floor(Math.random() * (106 - 101 + 1)) + 101;
+  const featuredMovie = await getFeaturedMovie(randomNum.toString());
 
   return (
     <div className='relative bg-gradient-to-b pb-8'>
