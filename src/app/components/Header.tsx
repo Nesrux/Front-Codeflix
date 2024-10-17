3c6008c1c5e13ab416bcfde21bcd174a668142d9
Nesrux <joaomarcosdevs@gmail.com>
2024-10-17 20:50:41 -0300

    feat: adiciona SearchForm

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,24 +1,51 @@
 'use client';
-import React from 'react';
+import { useSearchParams } from 'next/navigation';
+import { useRouter } from 'next/router';
+import React, { useState } from 'react';
 import { useScroll } from '../hooks/useScroll';
 import { Logo } from './Logo';
 import { NavLinks } from './NavLinks';
+import { SearchForm } from './SearchForm';
 import { UserProfile } from './UserProfile';
+import Link from 'next/link';
 
 export default function Header() {
   const isScrolled = useScroll();
+  const router = useRouter();
+  const params = useSearchParams();
+  const initialSearchTerm = params.get('title') || '';
+  const [searchTerm, setSearchTerm] = useState<string>(initialSearchTerm);
+
+  const onSearchTermChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const onSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+    event.preventDefault();
+    const newParams = new URLSearchParams(params.toString());
+    newParams.set('title', searchTerm);
+    router.push(`/search?${newParams.toString()}`);  };
 
   return (
     <header
-      className={`${isScrolled && 'bg-black'}
+    className={`${isScrolled && 'bg-black'}
     fixed top-0 z-50
     flex w-full items-center justify-between bg-gradient-to-t from-transparent to-black p-2 px-4 transition-all lg:px-16 lg:py-4`}
-    >
-      <div className='flex items-center space-x-2 md:space-x-8'>
+  >
+    <div className='flex items-center space-x-2 md:space-x-8'>
+      <Link href='/'>
         <Logo />
-        <NavLinks />
-      </div>
+      </Link>
+      <NavLinks />
+    </div>
+    <div className='flex items-center space-x-2 md:space-x-8'>
+      <SearchForm
+        onSearch={onSearch}
+        searchTerm={searchTerm}
+        onSearchTermChange={onSearchTermChange}
+      />
       <UserProfile />
-    </header>
+    </div>
+  </header>
   );
 }
