17e6dcae1b3e1f0bb27befaa65ae4eb1522a7b15
Nesrux <joaomarcosdevs@gmail.com>
2024-10-18 16:47:31 -0300

    refactor: refactor components

--- src/app/components/MovieInfo.tsx
+++ src/app/components/MovieInfo.tsx
@@ -1,13 +1,14 @@
-import {
-    ChevronDownIcon,
-    PlayIcon,
-    PlusIcon,
-    SpeakerWaveIcon,
-    UserGroupIcon,
-} from '@heroicons/react/24/outline';
+
 import Link from 'next/link';
-import { Movie } from '../types/movies';
+import {
+  ChevronDownIcon,
+  PlayIcon,
+  PlusIcon,
+  SpeakerWaveIcon,
+  UserGroupIcon,
+} from '@heroicons/react/24/outline';
 import { MovieRating } from './MovieRating';
+import { Movie } from '../types/movies';
 
 export const MovieInfo = ({ movie }: { movie: Movie }) => (
   <div className='absolute z-10 w-full rounded-b-md bg-zinc-800 p-2 shadow-md transition lg:p-4 '>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,12 @@
 import { Banner } from './components/Banner';
 import Header from './components/Header';
 import { MovieRow } from './components/MovieRow';
-import {
-  getFeaturedMovie,
-  getMovieById,
-  getMoviesByGenre,
-} from './services/MovieService';
+import { getFeaturedMovie, getMoviesByGenre } from './services/MovieService';
 
 export default async function Home() {
   const randomNum = Math.floor(Math.random() * (106 - 101 + 1)) + 101;
   const featuredMovie = await getFeaturedMovie(randomNum.toString());
-  const genres = ['Drame', 'Action', 'Comedy', 'Animation'];
+  const genres = ['Drama', 'Action', 'Comedy', 'Animation'];
 
   const movies = await Promise.all(
     genres.map(async (genre) => {
@@ -19,12 +15,10 @@ export default async function Home() {
     })
   );
 
-
   return (
     <div className='relative bg-gradient-to-b pb-8'>
       <Header />
-
-      <main className='relative mb-48 h-screen pl-4 lg:pl-16 '>
+      <main className='relative overflow-y-scroll p-8 pb-20 scrollbar-hide lg:px-16 '>
         <Banner movie={featuredMovie} />
         {movies.map((movie) => (
           <MovieRow
